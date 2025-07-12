147176b9643b31fb9497597589da9b549ef557be
Jared Holgate <jaredholgate@microsoft.com>
2025-07-12 14:29:01 +0100

    fix ip range

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "virtual_network_address_space" {
   description = "The address space for the virtual network."
   type        = string
-  default     = "10.0.0.0/24"
+  default     = "10.0.0.0/16"
 }
 
 variable "lab_instance_id" {
